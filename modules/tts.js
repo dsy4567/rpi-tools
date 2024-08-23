243a331096784a882bbd7f9fc8dad4b1a9485a4b
dsy4567 <dsy4567@outlook.com>
2024-08-24 00:13:33 +0800

    🚸  优化 espeak 朗读数字时的效果

--- modules/tts.js
+++ modules/tts.js
@@ -10,9 +10,10 @@ let /** @type {cp.ChildProcess} */ p, /** @type {NodeJS.Timeout} */ timeout;
 
 module.exports = {
     tts(/** @type {String} */ t) {
+        if (!t) return t;
         clearTimeout(timeout);
         timeout = setTimeout(() => {
-            log(t);
+            log((t = t.replaceAll(/[0-9]+/g, ".$&")));
             try {
                 p && p.kill();
                 p = cp.execFile("espeak", ["-v", TTSEspeakLanguage || "zh", t]);
